85f36796bbece94d3a5d59a6f46167f4c52e5de1
claytonrv <veras.clayton@gmail.com>
2021-01-24 18:13:38 -0300

    Recreated the project homepage and removed the old components and styles

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import '../styles/pages/HomePage.css'
+
+export default function HomePage(){
+
+    return (
+        <div id="home-page">
+            <h1>DIANA</h1>
+            <h3>YOUR NEW WAY TO CONSUME CONTENT</h3>
+            <a data-testid="arrow" href="/">
+                <div className="arrow">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </a>
+        </div>
+    )
+}
---
+++ src/tests/pages/HomePage.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+
+import { cleanup, render } from "@testing-library/react";
+
+import HomePage from "../../pages/HomePage";
+
+const setup = () => {
+  const wrapper = render(<HomePage />);
+  return { wrapper };
+};
+
+describe("Landing page", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("renders with correct information", () => {
+    const { wrapper } = setup();
+    const { getByText, getByTestId } = wrapper;
+    getByText("DIANA");
+    getByText("YOUR NEW WAY TO CONSUME CONTENT");
+    getByTestId("arrow")
+  });
+});
