5c6ef1d4ce9f582c230bb9619722bfd872ce89d7
Clayton R. Veras <veras.clayton@gmail.com>
2021-01-24 23:16:00 -0300

    Revert "issue_20/posts_page (#21)" (#22)
    This reverts commit 92e7c1c6a52746a77f67629fa1b61ad6c5c7bb6f.

--- src/pages/PostsPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import '../styles/pages/PostsPage.css';
-
-export default function PostsPage(){
-    return <div data-testid='posts-page' id='posts' className="posts"></div>
-}
