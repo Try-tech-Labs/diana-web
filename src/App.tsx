8d7de8b911bb7d53363aa28d2b9f0e1d675cdeb1
Clayton R. Veras <veras.clayton@gmail.com>
2021-01-24 23:16:15 -0300

    Recreated the project homepage and removed the old components and styles (#19)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-fluid App-body responsive">
+        <HomePage />
+      </div>           
+    </div>
+  );
+}
+
+export default App;
