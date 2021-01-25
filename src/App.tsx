fc3f4ab1b9d5fa16581e5d9ceeeb6c040f0e4b53
Clayton R. Veras <veras.clayton@gmail.com>
2021-01-24 23:22:51 -0300

    issue_20/posts_page_reopening (#23)
    * Created the posts page
    
    * Changed style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import HomePage from './pages/HomePage';
+import PostsPage from './pages/PostsPage';
 
 function App() {
   return (
     <div className="App">
       <div className="container-fluid App-body responsive">
         <HomePage />
+        <PostsPage />
       </div>           
     </div>
   );
