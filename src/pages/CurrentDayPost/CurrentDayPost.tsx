06268ed4c3667a2dc49df3baf729859770e65421
claytonrv <veras.clayton@gmail.com>
2021-03-24 19:00:01 -0300

    Refactored current day post component and posts payload mock

--- src/pages/CurrentDayPost/CurrentDayPost.tsx
+++ src/pages/CurrentDayPost/CurrentDayPost.tsx
@@ -11,42 +11,6 @@ import InformationDialog from '../../components/InformationDialog'
 
 import { getPostList } from '../../services/services'
 
-interface Tag {
-    id: number,
-    title: string,
-    creation_date: Date
-}
-
-interface Content {
-    id:number,
-    tags: Tag[],
-    title: string,
-    creation_date: Date,
-    author: string,
-    source: string,
-    source_url: string,
-    thumbnail: string,
-    content: string,
-    pos: number
-}
-
-interface Post {
-    id:number,
-    category: {
-        id: number,
-        title: string,
-        creation_date: Date, 
-        description: string,
-    },
-    videos: Content[],
-    tweets: Content[],
-    news: Content[],
-    trending_topics: TrendingTopic[],
-    title: string,
-    creation_date: Date,
-    search_count: number
-}
-
 interface TrendingTopic {
     trends_position: number,
     title: string
@@ -55,7 +19,6 @@ interface TrendingTopic {
 export default function CurrentDayPost(){
 
     const [showInformationDialog, setShowInformationDialog] = useState(false)
-    const [currentDayPost, setCurrentDayPost] = useState<Post[]>()
     const [trendingTopics, setTrendingTopics] = useState<TrendingTopic[]>([])
     const [dialogDataType, setDialogDataType] = useState('')
 
@@ -63,13 +26,12 @@ export default function CurrentDayPost(){
         const retrivePosts = async () => {
             const response = await getPostList()
             if (response){
-                setCurrentDayPost(response[0])
                 const topics = response[0].trending_topics
                 setTrendingTopics(topics.sort((itemA:TrendingTopic, itemB:TrendingTopic) => itemA.trends_position - itemB.trends_position))
             }
         }
         retrivePosts()
-    }, [getPostList, setCurrentDayPost])
+    }, [setTrendingTopics])
 
     const openInformationDialog = useCallback((dataType:string) => {
         setDialogDataType(dataType)
